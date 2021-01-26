added: Signal perceptron with cosines gradient descent for signal perceptron (doesnt work yet) fixed: Implementation details of sp.py

SPLinear/SP.py
@@ -11,14 +11,7 @@ m=3;  #Módule (base) Ej: si m=3, se genera 0,1,2,0,1,2,...
 k=2; #Number of nested loops. That is, number of variables
 aix=np.zeros([k]); #Array of indexes (to order them)
 aiw=np.zeros([k]); #Array of indexes (to order them)
-ni=1   #Number of Iterations
-
-for i in range(0, k, 1):
-	ni=ni*m
-
-print("ni=",ni)
-
-
+ni=m**k   #Number of Iterations
 n=k  #No. of variables
 nn=m**n #|m^k| domain space
 nnn=m**nn #|Delta|=|m^m^k| function space
@@ -35,7 +28,7 @@ for xi in range(0,ni,1):
 	for xj in range(0,k,1): #Generamos los índices
 		aix[xj]= int ( kx % m ); #Lo metemos en array 
 		kx=int(kx/m); #siguientes índices
-	print("aix=",aix)
+	#print("aix=",aix)
 	j=0;
 	#First Inner nested loop that generates all combinations of w for a signal
 	for wi in range(0,ni,1):
@@ -43,7 +36,7 @@ for xi in range(0,ni,1):
 		for wj in range(0,k,1): #Generamos los índices
 			aiw[wj]= int ( kw % m ) ; #Lo metemos en array 
 			kw=int(kw/m); #siguientes índices
-			print(i,j,A[i,j],"|",end='')
+			#print(i,j,A[i,j],"|",end='')
 		exponente=0
 		#Seconf Inner loop that  multiplies and sums
 		for ii in range(0,k,1):
@@ -102,9 +95,9 @@ def single_func():
 	p2=np.array([0,1,2,3,1,2,4,2,1,2,3,1,3,3,0,0,4,1,2,1,3,0,4,1,0]) 
 	h3=np.array([5,2,3,1,3,3,5,0,4,1,2,5,3,0,4,1,0,0,1,2,5,1,2,4,2,5,2,3,1,3,3,5,0,4,1,2,5,3,0,4,1,0,0,1,2,5,1,2,4,2,5,2,3,1,3,3,5,0,4,1,2,5,3,0,4,1,0,0,1,2,5,1,2,4,2,5,2,3,1,3,3,5,0,4,1,2,5,3,0,4,1,0,0,1,2,5,1,2,4,2,5,2,3,1,3,3,5,0,4,1,2,5,3,0,4,1,0,0,1,2,5,1,2,4,2,5,2,3,1,3,3,5,0,4,1,2,5,3,0,4,1,0,0,1,2,5,1,2,4,2,5,2,3,1,3,3,5,0,4,1,2,5,3,0,4,1,0,0,1,2,5,1,2,4,2,5,2,3,1,3,3,5,0,4,1,2,5,3,0,4,1,0,0,1,2,5,1,2,4,2,1,2,5,4,3,4,2,0,1,4,2,5,3,4,4,3]) 
 	o2=([0,1,2,3,1,2,5,2,1,6,3,1,5,3,7,0,4,1,2,1,3,0,7,1,7,0,1,2,3,1,2,5,2,1,6,3,1,5,3,7,0,4,1,2,1,3,0,7,1,7,1,3,4,7,6,1,2,3,7,0,2,5,3,7])
-	X = np.linalg.inv(A).dot(o2)
+	X = np.linalg.inv(A).dot(h3)
 	print("parameters for function:",X)
 all_func();
 #single_func();
-print(nnn)
+print("The function space size is: ",nnn)
 print(" End of lpn")

SPLinear/SP_cos.py
@@ -1,3 +1,5 @@
+#This code implements a variant of the signal perceptron using real sinousoids instead of analytic sinousoids
+
 import numpy as np
 
 #Solving a sistem of linear equations
@@ -7,13 +9,13 @@ import numpy as np
 #X2 = np.linalg.solve(A,B)
 #print(X2)
 
-m=8;  #Módule (base) Ej: si m=3, se genera 0,1,2,0,1,2,...
+m=3;  #Módule (base) Ej: si m=3, se genera 0,1,2,0,1,2,...
 k=2; #Number of nested loops. That is, number of variables
-aix=np.zeros([nc]); #Array of indexes (to order them)
-aiw=np.zeros([nc]); #Array of indexes (to order them)
+aix=np.zeros([k]); #Array of indexes (to order them)
+aiw=np.zeros([k]); #Array of indexes (to order them)
 ni=1   #Number of Iterations
 
-for i in range(0, nc, 1):
+for i in range(0, k, 1):
 	ni=ni*m
 
 print("ni=",ni)
@@ -23,61 +25,60 @@ n=k  #No. of variables
 nn=m**n #|m^k| domain space
 nnn=m**nn #|Delta|=|m^m^k| function space
 # Matrix for  
-A=np.zeros([nn,nn],dtype=complex)
-#A=np.zeros([nn,nn]) 
+A=np.zeros([nn,nn]) 
 divfrec=m-1
 i=0; j=0
 v=0; 
 #Function for generating the matrix for m^k perceptron
-def matrix_gen():
-	for xi in range(0,ni,1):
-		kx=xi;
-		for xj in range(0,k,1): #Generamos los índices
-			aix[xj]= int ( kx % m ); #Lo metemos en array 
-			kx=int(kx/m); #siguientes índices
-		print("aix=",aix)
-		j=0;
-		#First Inner nested loop that generates all combinations of w for a signal
-		for wi in range(0,ni,1):
-			kw=wi;
-			for wj in range(0,k,1): #Generamos los índices
-				aiw[wj]= int ( kw % m ) ; #Lo metemos en array 
-				kw=int(kw/m); #siguientes índices
-				print(i,j,A[i,j],"|",end='')
-			exponente=0
-			#Seconf Inner loop that  multiplies and sums
-			for ii in range(0,k,1):
-				exponente=exponente + aix[ii]*aiw[ii]
-				exponente=int(exponente)
-			#print("exponente=",exponente)
-			exponente=1j*np.pi*exponente/divfrec
-			#print(exponente)
-			#print(np.exp(exponente))
-			A[i,j]=np.exp(exponente)
-			#print(A[i,j])
-			j=j+1
-			#print("aiw=",aiw,"j=",j)
-			#for aj in range(0,nc,1):
-			#	print(i,j,A[i,j],"|",end='')
-			#	print()
-		i=i+1
-	print("A=",A)
-	#a1=A.astype(int) only uncomment this if you dont want the matrix to use the complex part
-	atrans= A.transpose()
-	print(atrans)
-	#h=np.matmul(A,atrans)
-	#print(h)
+
+for xi in range(0,ni,1):
+	kx=xi;
+	for xj in range(0,k,1): #Generamos los índices
+		aix[xj]= int ( kx % m ); #Lo metemos en array 
+		kx=int(kx/m); #siguientes índices
+	print("aix=",aix)
+	j=0;
+	#First Inner nested loop that generates all combinations of w for a signal
+	for wi in range(0,ni,1):
+		kw=wi;
+		for wj in range(0,k,1): #Generamos los índices
+			aiw[wj]= int ( kw % m ) ; #Lo metemos en array 
+			kw=int(kw/m); #siguientes índices
+			print(i,j,A[i,j],"|",end='')
+		exponente=0
+		#Seconf Inner loop that  multiplies and sums
+		for ii in range(0,k,1):
+			exponente=exponente + aix[ii]*aiw[ii]
+			exponente=int(exponente)
+		#print("exponente=",exponente)
+		exponente=np.pi*exponente/divfrec
+		#print(exponente)
+		#print(np.exp(exponente))
+		A[i,j]=np.cos(exponente)
+		#print(A[i,j])
+		j=j+1
+		#print("aiw=",aiw,"j=",j)
+		#for aj in range(0,nc,1):
+		#	print(i,j,A[i,j],"|",end='')
+		#	print()
+	i=i+1
+print("A=",A)
+#a1=A.astype(int) only uncomment this if you dont want the matrix to use the complex part
+atrans= A.transpose()
+print(atrans)
+#h=np.matmul(A,atrans)
+#print(h)
 #Function for calculating all posible functions from a m^k function space (Warning: this could fail if the function space is to big since it will require lots of memory)
 def all_func():
-	b=np.zeros([nnn,nn],dtype=complex)
-	alfa=np.zeros([nnn,nn],dtype=complex)
+	b=np.zeros([nnn,nn])
+	alfa=np.zeros([nnn,nn])
 	#Function that obtains the parameters of all possible functions
 	for i in range(0,nnn):
 		ii=i; r=i;
 		#base transformation function
 		for j in range(0,nn):
-			r=ii%base
-			ii=int(ii/base)
+			r=ii%m
+			ii=int(ii/m)
 			b[i,j]=r
 		#print(b[i])
 		X = np.linalg.inv(A).dot(b[i])
@@ -102,10 +103,9 @@ def single_func():
 	p2=np.array([0,1,2,3,1,2,4,2,1,2,3,1,3,3,0,0,4,1,2,1,3,0,4,1,0]) 
 	h3=np.array([5,2,3,1,3,3,5,0,4,1,2,5,3,0,4,1,0,0,1,2,5,1,2,4,2,5,2,3,1,3,3,5,0,4,1,2,5,3,0,4,1,0,0,1,2,5,1,2,4,2,5,2,3,1,3,3,5,0,4,1,2,5,3,0,4,1,0,0,1,2,5,1,2,4,2,5,2,3,1,3,3,5,0,4,1,2,5,3,0,4,1,0,0,1,2,5,1,2,4,2,5,2,3,1,3,3,5,0,4,1,2,5,3,0,4,1,0,0,1,2,5,1,2,4,2,5,2,3,1,3,3,5,0,4,1,2,5,3,0,4,1,0,0,1,2,5,1,2,4,2,5,2,3,1,3,3,5,0,4,1,2,5,3,0,4,1,0,0,1,2,5,1,2,4,2,5,2,3,1,3,3,5,0,4,1,2,5,3,0,4,1,0,0,1,2,5,1,2,4,2,1,2,5,4,3,4,2,0,1,4,2,5,3,4,4,3]) 
 	o2=([0,1,2,3,1,2,5,2,1,6,3,1,5,3,7,0,4,1,2,1,3,0,7,1,7,0,1,2,3,1,2,5,2,1,6,3,1,5,3,7,0,4,1,2,1,3,0,7,1,7,1,3,4,7,6,1,2,3,7,0,2,5,3,7])
-	X = np.linalg.inv(A).dot(b4)
-	#print("Function:",b4,X)
-#all_func();
-matrix_gen();
-single_func();
-print(nnn)
+	X = np.linalg.inv(A).dot(h3)
+	print("parameters for function:",X)
+all_func();
+#single_func();
+print("The function space size is: ",nnn)
 print(" End of lpn")