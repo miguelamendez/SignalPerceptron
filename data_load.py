c6d8a6090689fbabc259db17392e17fdf6f8fe6b
mpatsis <mpatsis13@gmail.com>
2021-07-26 17:06:00 +0300

    Some code cleaning for running experiments and data loading.

--- data_load.py
+++ data_load.py
@@ -1,37 +1,73 @@
 import numpy as np
 import pickle
 
-def gen_finite_array(m,length,size):
-    yki=[]
-    aiy=np.zeros([length]);
-    for i in range(0,size,1):
-        ky=i;
-        for j in range(0,length,1):
-            aiy[j]= int ( ky % m );  
-            ky=int(ky/m); 
-        yt=[]
+def gen_finite_array(m, length, size):
+    """Generates and returns a finite size array of functions based on the specified parameters.
+
+    Args:
+        m ([type]): [description]
+        length ([type]): [description]
+        size ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    yki = []
+    aiy = np.zeros([length]);
+
+    for i in range(size):
+        ky = i;
+        for j in range(length):
+            aiy[j] = int( ky % m );  
+            ky = int( ky / m ); 
+        yt = []
         for l in aiy:
             yt.append(l)
         yki.append(yt)
+    
     y = np.asarray(yki)
     return y
 
 
-def data_gen(m,k,func_samples=[]):
+def data_gen(m, k, func_samples=[]):
+    """[summary]
+
+    Args:
+        m ([type]): [description]
+        k ([type]): [description]
+        func_samples (list, optional): [description]. Defaults to [].
+
+    Returns:
+        [type]: [description]
+    """
+
     if bool(func_samples):
         #Creating n random functions
-        Y=np.random.randint(m, size=(func_samples,m**k))
+        Y = np.random.randint(m, size=(func_samples, m**k))
     else:
         #Creating all possible functions 
-        Y=gen_finite_array(m,length=m*k,size=m**(m**k))
+        Y = gen_finite_array(m, length=m*k, size=m**(m**k))
+    
     #Creating Dataset that is all possible combinations of the inputs
-    X=gen_finite_array(m,length=k,size=(m**k))
+    X = gen_finite_array(m, length=k, size=(m**k))
     return X, Y
 
-def partial_data_gen():
-    
-    return train_X,train_Y,test_X,test_Y
 
+def partial_data_gen():
+    """Stub function used for generation of train and test sets.
+
+    Returns:
+        [type]: [description]
+    """
+    return train_X, train_Y, test_X, test_Y
+
+
+# TODO: Rafael: This seems to be unimplemented. 
 def mnist_data_gent():
+    """Generates data for experiments based on the MNIST database.
+
+    Returns:
+        [type]: [description]
+    """
     file = open("Fruits.obj",'rb')
-    return train_X,train_Y,test_X,test_Y
+    return train_X, train_Y, test_X, test_Y
