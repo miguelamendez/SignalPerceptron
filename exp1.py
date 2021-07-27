Some bugs fixed: bug1 Learning now is done correctly in exp2 bug2 The loss for numpy in the graphs now is displayed correctly (forgot to do the average instead of the sum) Still has issues: when plotting the functions it shows the correct graph but when saving the graph it does something weird.

exp1.py
@@ -182,8 +182,8 @@ def full_analysis_train():
     print("Single function learning")
     #Training Hyperparameters:
     #----------------------------------------------------------
-    epochs = 1000
-    lr = .01
+    epochs = 100
+    lr = .1
     optimizer_name="sgd"
     loss_fn_1 = nn.MSELoss()
     loss_fn_2 = nn.MSELoss()
@@ -236,8 +236,8 @@ def full_analysis_train():
     print("Multiple function learning")
     #Training Hyperparameters:
     #----------------------------------------------------------
-    epochs = 1000
-    lr = .01
+    #epochs = 100
+    #lr = .1
     optimizer_name="SGD"
     loss_fn_pt1 = nn.MSELoss()
     loss_fn_pt2 = nn.MSELoss()
@@ -251,41 +251,57 @@ def full_analysis_train():
     print("Parameters: ")
     print("Epochs: ",epochs," lr: ",lr," optimizer: ",optimizer_name)
     #----------------------------------------------------------
+    all_mh=[]
     print("Signal Perceptron numpy")
-    total_hist1,final_loss1,learned_epochs1=train_mh_numpy(x_train=x,y_train=y,model=sp_np_mh,epochs=epochs,learning_rate=lr,loss_fn=MSE_Loss)
-    print(final_loss1,learned_epochs1)
+    total_hist6,final_loss6,learned_epochs6=train_mh_numpy(x_train=x,y_train=y,model=sp_np_mh,epochs=epochs,learning_rate=lr,loss_fn=MSE_Loss)
+    a=total_hist6[0]
+    all_mh.append(a)
+    print(final_loss6,learned_epochs6)
     print("Real Signal Perceptron numpy")
-    total_hist2,final_loss2,learned_epochs2=train_mh_numpy(x_train=x,y_train=y,model=rsp_np_mh,epochs=epochs,learning_rate=lr,loss_fn=MSE_Loss)
-    print(final_loss2,learned_epochs2)
+    total_hist7,final_loss7,learned_epochs7=train_mh_numpy(x_train=x,y_train=y,model=rsp_np_mh,epochs=epochs,learning_rate=lr,loss_fn=MSE_Loss)
+    a=total_hist7[0]
+    all_mh.append(a)
+    print(final_loss7,learned_epochs7)
     print("Real Signal Perceptron pytorch")
-    total_hist3,final_loss3,learned_epochs3=train_mh_pytorch(x_train=x_train,y_train=y_train,model=rsp_mh,PATH=PATH5,epochs=epochs,optimizer=optimizer2,loss_fn=loss_fn_pt1)
-    print(final_loss3,learned_epochs3)
+    total_hist8,final_loss8,learned_epochs8=train_mh_pytorch(x_train=x_train,y_train=y_train,model=rsp_mh,PATH=PATH5,epochs=epochs,optimizer=optimizer2,loss_fn=loss_fn_pt1)
+    a=total_hist8[0]
+    all_mh.append(a)
+    print(final_loss8,learned_epochs8)
     print("Fourier Signal Perceptron pytorch")
-    total_hist4,final_loss4,learned_epochs4=train_mh_pytorch(x_train=x_train,y_train=y_train,model=fsp_mh,PATH=PATH4,epochs=epochs,optimizer=optimizer1,loss_fn=loss_fn_pt2)
-    print(final_loss4,learned_epochs4)
+    total_hist9,final_loss9,learned_epochs9=train_mh_pytorch(x_train=x_train,y_train=y_train,model=fsp_mh,PATH=PATH4,epochs=epochs,optimizer=optimizer1,loss_fn=loss_fn_pt2)
+    a=total_hist9[0]
+    all_mh.append(a)
+    print(final_loss9,learned_epochs9)
     print(" Multilayer Perceptron pytorch")
-    total_hist5,final_loss5,learned_epochs5=train_mh_pytorch(x_train=x_train,y_train=y_train,model=mlp_mh,PATH=PATH6,epochs=epochs,optimizer=optimizer3,loss_fn=loss_fn_pt3)
-    print(final_loss5,learned_epochs5)
+    total_hist,final_loss,learned_epochs=train_mh_pytorch(x_train=x_train,y_train=y_train,model=mlp_mh,PATH=PATH6,epochs=epochs,optimizer=optimizer3,loss_fn=loss_fn_pt3)
+    a=total_hist[0]
+    all_mh.append(a)
+    print(final_loss,learned_epochs)
     #Ploting loss for trained networks. 
     title1='Training loss of '+str(len(total_hist1))+' functions of the m:'+str(m)+',k:'+str(k)+' function space with SP_np'
     title2='Training loss of '+str(len(total_hist2))+' functions of the m:'+str(m)+',k:'+str(k)+' function space with RSP_np'
     title3='Training loss of '+str(len(total_hist3))+' functions of the m:'+str(m)+',k:'+str(k)+' function space with SP_pt'
     title4='Training loss of '+str(len(total_hist4))+' functions of the m:'+str(m)+',k:'+str(k)+' function space with FSP_pt'
     title5='Training loss of '+str(len(total_hist5))+' functions of the m:'+str(m)+',k:'+str(k)+' function space with MLP_pt'
+    title='Training loss for learning the m:'+str(m)+',k:'+str(k)+' function space'
     dir_section="run1/sgd/"
-    image_path1="data/experiments/exp1/"+dir_section+"sp_np_mh.png"
-    image_path2="data/experiments/exp1/"+dir_section+"rsp_np_mh.png"
-    image_path3="data/experiments/exp1/"+dir_section+"rsp_pt_mh.png"
-    image_path4="data/experiments/exp1/"+dir_section+"fsp_pt_mh.png"
-    image_path5="data/experiments/exp1/"+dir_section+"mlp_pt_mh.png"
+    image_path6="data/experiments/exp1/"+dir_section+"sp_np_mh.png"
+    image_path7="data/experiments/exp1/"+dir_section+"rsp_np_mh.png"
+    image_path8="data/experiments/exp1/"+dir_section+"rsp_pt_mh.png"
+    image_path9="data/experiments/exp1/"+dir_section+"fsp_pt_mh.png"
+    image_path="data/experiments/exp1/"+dir_section+"mlp_pt_mh.png"
+    image_path_mh="data/experiments/exp1/"+dir_section+"all_mh.png"
     #image_path3="data/experiments/exp1/run1/adam/rsp_pt_mh"
     #image_path4="data/experiments/exp1/run1/adam/fsp_pt_mh"
     #image_path5="data/experiments/exp1/run1/adam/mlp_pt_mh"
-    functions_plot(total_hist1,title1,image_path1)
-    functions_plot(total_hist2,title2,image_path2)
-    functions_plot(total_hist3,title3,image_path3)
-    functions_plot(total_hist4,title4,image_path4)
-    functions_plot(total_hist5,title5,image_path5)
+    functions_plot(total_hist6,title1,image_path6)
+    functions_plot(total_hist7,title2,image_path7)
+    functions_plot(total_hist8,title3,image_path8)
+    functions_plot(total_hist9,title4,image_path9)
+    functions_plot(total_hist,title5,image_path)
+    labels=["SP_np","RSP_np","RSP_pt","FSP_pt","MLP_pt"]
+    functions_plot(all_mh,title,image_path_mh,labels)
+    #print(all_mh)
     #################################################################################################################################
     #################################################################################################################################
     #################################################################################################################################

exp2.py
@@ -61,7 +61,7 @@ mlp1 = MLP1_mnist().to(device)
 mlp2 = MLP2_mnist().to(device)
 #Saving Initial values of parameters
 PATH="data/models/idm_FSP128_mnist.pt"
-torch.save(fsp.state_dict(),PATH)
+torch.save(fsp128.state_dict(),PATH)
 PATH1="data/models/idm_FSP512_mnist.pt"
 torch.save(fsp.state_dict(),PATH1)
 PATH2="data/models/idm_MLP1_mnist.pt"
@@ -164,16 +164,20 @@ print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
 #Training Hyperparameters:
 #----------------------------------------------------------
 epochs = 9
+lr=.001
 loss_fn = nn.CrossEntropyLoss()
-#optimizer = torch.optim.Adam(fsp128.parameters(), lr=.001)
-#optimizer1 = torch.optim.Adam(fsp.parameters(), lr=.001)
-#optimizer2 = torch.optim.Adam(mlp1.parameters(), lr=.001)
-#optimizer3 = torch.optim.Adam(mlp2.parameters(), lr=.001)
-optimizer = torch.optim.SGD(fsp128.parameters(), lr=.001)
-optimizer1 = torch.optim.SGD(fsp.parameters(), lr=.001)
-optimizer2 = torch.optim.SGD(mlp1.parameters(), lr=.001)
-optimizer3 = torch.optim.SGD(mlp2.parameters(), lr=.001)
+optimizer = torch.optim.Adam(fsp128.parameters(), lr=lr)
+optimizer1 = torch.optim.Adam(fsp.parameters(), lr=lr)
+optimizer2 = torch.optim.Adam(mlp1.parameters(), lr=lr)
+optimizer3 = torch.optim.Adam(mlp2.parameters(), lr=lr)
+#optimizer = torch.optim.SGD(fsp128.parameters(), lr=lr)
+#optimizer1 = torch.optim.SGD(fsp.parameters(), lr=lr)
+#optimizer2 = torch.optim.SGD(mlp1.parameters(), lr=lr)
+#optimizer3 = torch.optim.SGD(mlp2.parameters(), lr=lr)
+print("Optimizer: Adam ,lr:" ,lr)
+#print("Optimizer: SGD ,lr:" ,lr)
 #----------------------------------------------------------
+
 print("Training models with MNIST DATASET :")
 print("Fourier Signal Perceptron 128")
 optimal_epoch=[]
@@ -240,6 +244,8 @@ print("Optimal  epoch:")
 print(optimal_epoch)
 
 print("Training models with FashionMNIST DATASET :")
+#Loading initial parameters
+fsp128.load_state_dict(torch.load(PATH))
 fsp.load_state_dict(torch.load(PATH1))
 mlp1.load_state_dict(torch.load(PATH2))
 mlp2.load_state_dict(torch.load(PATH3))

train.py
@@ -41,7 +41,7 @@ def train_numpy(x_train,y_train,model,epochs,learning_rate,loss_fn):
         for j in range(0,epochs):
             pred,signals=model.forward(x_train)
             loss = loss_fn(pred, i)
-            loss = np.sum(loss)
+            loss = np.mean(loss)
             sp.GD_MSE_SP_step(i, x_train, model,learning_rate)
             history_train.append([j,loss])
             if not bool(learned_epoch):
@@ -83,7 +83,7 @@ def train_mh_numpy(x_train,y_train,model,epochs,learning_rate,loss_fn):
     for j in range(0,epochs):
         pred,signals=model.forward(x_train)
         loss = loss_fn(pred, y_train)
-        loss = np.sum(loss)
+        loss = np.mean(loss)
         sp.GD_MSE_SP_step(y_train, x_train, model,learning_rate)
         history_train.append([j,loss])
         if not bool(learned_epoch):
@@ -98,20 +98,17 @@ def train_mnist(dataloader, model, loss_fn, optimizer,device):
     size = len(dataloader.dataset)
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
-
         # Compute prediction error
         pred = model(X)
         loss = loss_fn(pred, y)
-
         # Backpropagation
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-            return loss 
+    return loss 
 
 def test_mnist(dataloader, model, loss_fn,device):
     size = len(dataloader.dataset)